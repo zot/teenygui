debb3bc9cc746cfd31dd2e72af6bfc3b58327858
Bill Burdick <bill.burdick@gmail.com>
2020-06-25 10:18:20 -0500

    initial commit

---
+++ app.ts
@@ -0,0 +1,96 @@
+/*
+   Copyright (C) 2020, Bill Burdick
+
+   The MIT License (MIT)
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+import * as gui from './teenygui.js';
+import { $, $nodes, $find, ObId } from './teenygui.js';
+
+const objects = {};
+var nextOid = 0;
+
+const names = [
+    'Bubba', 'Chortle', 'Gruntudley', 'Quibsome',
+    'Malubador', 'Frangwila', 'Smeep', 'Schmondle',
+];
+const streets = [
+    'Elm', 'Pine', 'Brick', 'Toad',
+];
+var nameCount = 0;
+
+class Person {
+    constructor(obj: object) {
+        Object.assign(this, obj);
+    }
+}
+
+function update(id: ObId, field: string, value: string) {
+    const person = objects[id];
+
+    person[field] = value;
+    console.log(`PERSON CHANGED[${field}]: ${id}: ${JSON.stringify(person)}`);
+}
+
+function addObject(obj: any) {
+    obj.id = nextOid++;
+    objects[obj.id] = obj;
+    return obj;
+}
+
+const deleteObject = (id: ObId) => delete objects[id];
+
+function randomPerson() {
+    const name = names[Math.floor(Math.random() * names.length)];
+    const addr = Math.ceil(Math.random() * 1000);
+    const street = streets[Math.floor(Math.random() * streets.length)];
+
+    return addObject(new Person({
+        name: `${name} ${++nameCount}`,
+        address: `${addr} ${street}`
+    }));
+}
+
+function deletePerson(id: ObId) {
+    for (let node of $nodes(id)) {
+        node.remove();
+    }
+    deleteObject(id);
+}
+
+function makePeep() {
+    const person = randomPerson();
+    const id = person.id;
+    // dispay object and bind fields
+    const node = gui.bind(id, gui.clone('#peep'), update);
+
+    // bind delete icon for this person
+    ($find(node, '[name=close]') as HTMLElement).onclick = () => deletePerson(id)
+    // add node to page
+    $('#peeps').append(node);
+}
+
+function init() {
+    gui.initGui((id) => objects[id]);
+    $('#button-new-peep').onclick = () => makePeep();
+}
+
+window.onload = init;
